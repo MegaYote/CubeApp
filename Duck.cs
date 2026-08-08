c755daa9e51e21524241b4293910bf5af6a2d220
James <cubuild.net@gmail.com>
2026-08-07 22:01:20 -0700

    Ducks and coyotes fit through 1x1 openings: duck collision 0.7x1.4 -> 0.9x0.95, coyote 1.2x1.6 -> 0.9x0.95. Both verified pathing through a 1-block-wide/tall hole in a wall.

--- Duck.cs
+++ Duck.cs
@@ -54,8 +54,8 @@ namespace CubeApp
     /// </summary>
     public sealed class Duck : MobEntity
     {
-        public const float DuckWidth = 0.7f;
-        public const float DuckHeight = 1.4f;
+        public const float DuckWidth = 0.9f;
+        public const float DuckHeight = 0.95f;
 
         public Duck(Point3D position, float yaw) : base(position, yaw)
         {
